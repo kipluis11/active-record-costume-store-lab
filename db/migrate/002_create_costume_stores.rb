07539d5fbc547e43f654af72fc1f1cf86afec8c1
kip_parcell <kipluis@gmail.com>
2020-12-05 12:37:29 -0700

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,3 +1,17 @@
 # Create your costume_stores migration here
 
-# !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+# !!! Before you run rake db:migrate, remember to fill out the other migget an error resulting from the blank migration files.ration files -- otherwise you'll 
+
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+    def change
+        create_table :costume_stores do |s|
+            s.string :name
+            s.string :location
+            s.integer :costume_inventory
+            s.integer :num_of_employees
+            s.boolean :still_in_business
+            s.datetime :opening_time
+            s.datetime :closing_time
+        end
+    end
+end
